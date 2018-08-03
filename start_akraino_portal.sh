9480a2ab3026f6fd036b577edef56cd2f3f2dd6e
davidplunkett <dp7642@att.com>
2018-08-03 05:27:18 -0400

    added ip detection, portal config, formating
    Change-Id: I7c635bade049592cc3e2762be738ff9fc5d9defd
    Signed-off-by: davidplunkett <dp7642@att.com>

--- start_akraino_portal.sh
+++ start_akraino_portal.sh
@@ -16,205 +16,126 @@
 #
 # start_akraino_portal.sh - start the Akraino portal on the regional controller
 #
-DB_IMAGE="nexus3.akraino.org:10003/akraino_schema_db:$1"
-WF_IMAGE=nexus3.akraino.org:10003/akraino-camunda-workflow-engine:$2
-PT_IMAGE=nexus3.akraino.org:10003/akraino-portal:$3
-# Login if needed
+
+DB_VERSION=${1:-"1.0.0-SNAPSHOT"}
+WF_VERSION=${2:-"0.0.1-SNAPSHOT"}
+PT_VERSION=${3:-"0.0.1-SNAPSHOT"}
+
+DB_IMAGE="nexus3.akraino.org:10003/akraino_schema_db:$DB_VERSION"
+WF_IMAGE="nexus3.akraino.org:10003/akraino-camunda-workflow-engine:$WF_VERSION"
+PT_IMAGE="nexus3.akraino.org:10003/akraino-portal:$PT_VERSION"
+LD_IMAGE="openmicroscopy/apacheds"
+
+NEXUS_URL="https://nexus.akraino.org"
+PORTAL_URL="$NEXUS_URL/service/local/artifact/maven/redirect?r=snapshots&g=org.akraino&a=portal_user_interface&v=$PT_VERSION&e=war"
+TEMPEST_URL="$NEXUS_URL/service/local/artifact/maven/redirect?r=snapshots&g=org.akraino.test_automation&a=test_automation&v=1.0.0-SNAPSHOT&e=tgz"
+YAML_BUILDS_URL="$NEXUS_URL/service/local/artifact/maven/redirect?r=snapshots&g=org.akraino.yaml_builds&a=yaml_builds&v=1.0.0-SNAPSHOT&e=tgz"
+ONAP_URL="$NEXUS_URL/service/local/artifact/maven/redirect?r=snapshots&g=org.akraino.addon-onap&a=onap-amsterdam-regional-controller-master&v=1.0.0-SNAPSHOT&e=tgz"
+SAMPLE_VNF_URL="$NEXUS_URL/service/local/artifact/maven/redirect?r=snapshots&g=org.akraino.sample_vnf&a=sample_vnf&v=1.0.0-SNAPSHOT&e=tgz"
+AIRSHIPINABOTTLE_URL="$NEXUS_URL/service/local/artifact/maven/redirect?r=snapshots&g=org.akraino.airshipinabottle_deploy&a=airshipinabottle_deploy&v=1.0.0-SNAPSHOT&e=tgz"
+REDFISH_URL="$NEXUS_URL/service/local/artifact/maven/redirect?r=snapshots&g=org.akraino.redfish&a=redfish&v=1.0.0-SNAPSHOT&e=tgz"
+
+LDAP_FILE_HOME="/opt/akraino/ldap"
+TEMPEST_HOME="/opt/akraino/tempest"
+YAML_BUILDS_HOME="/opt/akraino/yaml_builds"
+ONAP_HOME="/opt/akraino/onap"
+SAMPLE_VNF_HOME="/opt/akraino/sample_vnf"
+AIRSHIPINABOTTLE_HOME="/opt/akraino/airshipinabottle_deploy"
+REDFISH_HOME="/opt/akraino/redfish"
+
+# Find the primary ip address (the one used to access the default gateway)
+# This ip will be used for communication between the containers
+IP=$(ip route get 8.8.8.8 | grep -o "src .*$" | cut -f 2 -d ' ')
+
+apt-get install unzip -y
 
 # PostgreSQL
 docker run \
-	--detach \
-	--publish 6432:5432 \
+        --detach \
+        --publish 6432:5432 \
         --volume /var/lib/postgres:/var/lib/postgresql/data \
-	--network=bridge \
-	--name postgres \
-	--env "POSTGRES_USER=admin" \
-	--env "POSTGRES_PASSWORD=abc123" \
-	$DB_IMAGE
+        --network=bridge \
+        --name postgres \
+        --env "POSTGRES_USER=admin" \
+        --env "POSTGRES_PASSWORD=abc123" \
+        $DB_IMAGE
 
-#ldap
-LDAP_FILE_HOME="/opt/akraino/LDAP_FILES"
-# To get latest artifacts from nexus
-apt-get install unzip -y
-version=`curl -s https://nexus.akraino.org/content/repositories/snapshots/org/akraino/portal_user_interface/maven-metadata.xml | grep version | sed "s/.*<version>\([^<]*\)<\/version>.*/\1/" | head -4 | tail -1`
-build=`curl -s  https://nexus.akraino.org/content/repositories/snapshots/org/akraino/portal_user_interface/$version/maven-metadata.xml | grep '<value>' | head -1 | sed "s/.*<value>\([^<]*\)<\/value>.*/\1/"`
-tar=portal_user_interface-$build.war
-path="https://nexus.akraino.org/content/repositories/snapshots/org/akraino/portal_user_interface/$version"
-portal_user_interface_url="$path/$tar"
-
+# LDAP
 echo "Download ldap ldif files"
-#checking if directory exist or not
-if [ -d $LDAP_FILE_HOME ]; then
-   rm -rf $LDAP_FILE_HOME
-   mkdir -p $LDAP_FILE_HOME
-else
-   mkdir -p $LDAP_FILE_HOME
-fi
+rm -rf $LDAP_FILE_HOME
+wget -q "$PORTAL_URL" -O /tmp/portal_user_interface.war
+unzip -oj /tmp/portal_user_interface.war WEB-INF/classes/*.ldif -d $LDAP_FILE_HOME
 
-wget $portal_user_interface_url -O /tmp/portal_user_interfaces.war
-
-if [[ -d "/tmp/portal_user_interface" ]];
-then
-   rm -rf /tmp/portal_user_interface/*
-else
-   mkdir -p /tmp/portal_user_interface
-fi
-
-unzip /tmp/portal_user_interfaces.war -d /tmp/portal_user_interface
-cp -rf  /tmp/portal_user_interface/WEB-INF/classes/*.ldif $LDAP_FILE_HOME/
-
-docker run --name akraino_ldap -d -p 10389:10389 -e APACHEDS_INSTANCE=akraion -v $LDAP_FILE_HOME/config.ldif:/bootstrap/conf/config.ldif:ro  openmicroscopy/apacheds
-docker cp $LDAP_FILE_HOME/akrainousers.ldif akraino_ldap:/bootstrap/conf
+docker run \
+        --detach \
+        --publish 10389:10389 \
+        --volume $LDAP_FILE_HOME/config.ldif:/bootstrap/conf/config.ldif:ro \
+        --volume $LDAP_FILE_HOME/akrainousers.ldif:/bootstrap/conf/akrainousers.ldif:ro \
+        --name akraino-ldap \
+        --env "APACHEDS_INSTANCE=akraino" \
+        $LD_IMAGE
 sleep 15
-docker exec akraino_ldap /bin/bash -c "ldapadd -v -h $4:10389 -c -x -D uid=admin,ou=system -w secret -f /bootstrap/conf/akrainousers.ldif"
-#intialize DB
+docker exec akraino-ldap /bin/bash -c "ldapadd -v -h $IP:10389 -c -x -D uid=admin,ou=system -w secret -f /bootstrap/conf/akrainousers.ldif"
+
+# Intialize DB
 sleep 30
 docker exec postgres /bin/bash -c "cp -rf /akraino-j2templates/* /var/lib/postgresql/data/"
 docker exec postgres /bin/bash -c "psql -h localhost -p 5432 -U postgres -f /akraino-db_0524.sql"
 
-
-### TODO - initialize filesystem
-
 # Workflow
 docker run \
-	--detach \
-	--publish 8073:8080 \
-	--network=bridge \
-	--name akraino-workflow \
-	$WF_IMAGE
+        --detach \
+        --publish 8073:8015 \
+        --network=bridge \
+        --name akraino-workflow \
+        $WF_IMAGE
 
 # Portal
 docker run \
-	--detach \
-	--publish 8080:8080 \
-	--network=bridge \
+        --detach \
+        --publish 8080:8080 \
+        --network=bridge \
         --volume /opt/tomcat/logs:/usr/local/tomcat/logs \
         --volume /opt/aec_poc/aic-clcp-manifests/site/site80:/usr/local/site80 \
-	--name akraino-portal \
-	$PT_IMAGE
+        --volume /opt/akraino/server-build:/opt/akraino/server-build \
+        --name akraino-portal \
+        $PT_IMAGE
 
-TEMPEST_HOME="/opt/akraino/tempest"
-YAML_BUILDS_HOME="/opt/akraino/yaml_builds"
-# To get latest artifacts from nexus
-version=`curl -s https://nexus.akraino.org/content/repositories/snapshots/org/akraino/test_automation/test_automation/maven-metadata.xml | grep version | sed "s/.*<version>\([^<]*\)<\/version>.*/\1/" | head -4 | tail -1`
-build=`curl -s  https://nexus.akraino.org/content/repositories/snapshots/org/akraino/test_automation/test_automation/$version/maven-metadata.xml | grep '<value>' | head -1 | sed "s/.*<value>\([^<]*\)<\/value>.*/\1/"`
-tar=test_automation-$build.tgz
-path="https://nexus.akraino.org/content/repositories/snapshots/org/akraino/test_automation/test_automation/$version"
-TEMPEST_URL="$path/$tar"
-#TEMPEST_URL="https://nexus.akraino.org/content/repositories/snapshots/org/akraino/tempest/tempest-master/0.0.1-SNAPSHOT/tempest-master-0.0.1-20180611.172116-1.tar"
+# Portal configuration
+echo "Updating portal configuration"
+docker exec akraino-portal /bin/bash -c "sed -i -e \"s|localhost|$IP|g\" /usr/local/tomcat/webapps/AECPortalMgmt/App.Config.js"
+docker exec akraino-portal /bin/bash -c "sed -i -e \"s|localhost|$IP|g\" /usr/local/tomcat/webapps/AECPortalMgmt/WEB-INF/classes/app.properties"
+
 
 echo "Setting up tempest content/repositories"
-#checking if directory exist or not
-if [ -d $TEMPEST_HOME ]; then
-   rm -rf $TEMPEST_HOME
-   mkdir -p $TEMPEST_HOME
-else
-   mkdir -p $TEMPEST_HOME
-fi
-
-wget -q $TEMPEST_URL -O /opt/akraino/test_automation.tgz
-tar -xzf /opt/akraino/test_automation.tgz -C $TEMPEST_HOME
-#mv $TEMPEST_HOME/openstack_tempest/* $TEMPEST_HOME/
-#rm -rf $TEMPEST_HOME/openstack_tempest
-rm -rf /opt/akraino/test_automation.tgz
-
-# To get latest artifacts from nexus
-yaml_builds_version=`curl -s https://nexus.akraino.org/content/repositories/snapshots/org/akraino/yaml_builds/yaml_builds/maven-metadata.xml | grep version | sed "s/.*<version>\([^<]*\)<\/version>.*/\1/" | head -4 | tail -1`
-yaml_builds_build=`curl -s  https://nexus.akraino.org/content/repositories/snapshots/org/akraino/yaml_builds/yaml_builds/$yaml_builds_version/maven-metadata.xml | grep '<value>' | head -1 | sed "s/.*<value>\([^<]*\)<\/value>.*/\1/"`
-yaml_builds_tar=yaml_builds-$yaml_builds_build.tgz
-yaml_builds_path="https://nexus.akraino.org/content/repositories/snapshots/org/akraino/yaml_builds/yaml_builds/$yaml_builds_version"
-YAML_BUILDS_URL="$yaml_builds_path/$yaml_builds_tar"
+rm -rf $TEMPEST_HOME
+mkdir -p $TEMPEST_HOME
+wget -q "$TEMPEST_URL" -O - | tar -xz -C $TEMPEST_HOME
 
 echo "Setting up yaml builds content/repositories"
-#checking if directory exist or not
-if [ -d $YAML_BUILDS_HOME ]; then
-   rm -rf $YAML_BUILDS_HOME
-   mkdir -p $YAML_BUILDS_HOME
-else
-   mkdir -p $YAML_BUILDS_HOME
-fi
-
-wget -q $YAML_BUILDS_URL -O /opt/akraino/yaml_builds.tgz
-tar -xzf /opt/akraino/yaml_builds.tgz  -C $YAML_BUILDS_HOME
-rm -rf /opt/akraino/yaml_builds.tgz
-
-
-ONAP_HOME="/opt/akraino/onap"
-onap_version=`curl -s https://nexus.akraino.org/content/repositories/snapshots/org/akraino/addon-onap/onap-amsterdam-regional-controller-master/maven-metadata.xml | grep version | sed "s/.*<version>\([^<]*\)<\/version>.*/\1/" | head -4 | tail -1`
-onap_build=`curl -s  https://nexus.akraino.org/content/repositories/snapshots/org/akraino/addon-onap/onap-amsterdam-regional-controller-master/$onap_version/maven-metadata.xml | grep '<value>' | head -1 | sed "s/.*<value>\([^<]*\)<\/value>.*/\1/"`
-onap_tar=onap-amsterdam-regional-controller-master-$onap_build.tgz
-onap_path="https://nexus.akraino.org/content/repositories/snapshots/org/akraino/addon-onap/onap-amsterdam-regional-controller-master/$onap_version"
-ONAP_URL="$onap_path/$onap_tar"
+rm -rf $YAML_BUILDS_HOME
+mkdir -p $YAML_BUILDS_HOME
+wget -q "$YAML_BUILDS_URL" -O - | tar -xz -C $YAML_BUILDS_HOME
 
 echo "Setting up ONAP content/repositories"
-#checking if directory exist or not
-if [ -d $ONAP_HOME ]; then
-   rm -rf $ONAP_HOME
-   mkdir -p $ONAP_HOME
-else
-   mkdir -p $ONAP_HOME
-fi
-wget -q $ONAP_URL -O /opt/akraino/onap.tgz
-tar -xzf /opt/akraino/onap.tgz  -C $ONAP_HOME
-rm -rf /opt/akraino/onap.tgz
-
-SAMPLE_VNF="/opt/akraino/sample_vnf"
-sample_vnf_version=`curl -s https://nexus.akraino.org/content/repositories/snapshots/org/akraino/sample_vnf/sample_vnf/maven-metadata.xml | grep version | sed "s/.*<version>\([^<]*\)<\/version>.*/\1/" | head -4 | tail -1`
-sample_vnf_build=`curl -s  https://nexus.akraino.org/content/repositories/snapshots/org/akraino/sample_vnf/sample_vnf/$sample_vnf_version/maven-metadata.xml | grep '<value>' | head -1 | sed "s/.*<value>\([^<]*\)<\/value>.*/\1/"`
-sample_vnf_tar=sample_vnf-$sample_vnf_build.tgz
-sample_vnf_path="https://nexus.akraino.org/content/repositories/snapshots/org/akraino/sample_vnf/sample_vnf/$sample_vnf_version"
-SAMPLE_VNF_URL="$sample_vnf_path/$sample_vnf_tar"
+rm -rf $ONAP_HOME
+mkdir -p $ONAP_HOME
+wget -q "$ONAP_URL" -O - | tar -xz -C $ONAP_HOME
 
 echo "Setting up sample vnf content/repositories"
-#checking if directory exist or not
-if [ -d $SAMPLE_VNF ]; then
-   rm -rf $SAMPLE_VNF
-   mkdir -p $SAMPLE_VNF
-else
-   mkdir -p $SAMPLE_VNF
-fi
-
-wget -q $SAMPLE_VNF_URL -O /opt/akraino/sample_vnf.tgz
-tar -xzf /opt/akraino/sample_vnf.tgz -C $SAMPLE_VNF
-rm -rf /opt/akraino/sample_vnf.tgz
-
-AIRSHIPINABOTTLE="/opt/akraino/airshipinabottle_deploy"
-airshipinabottle_deploy_version=`curl -s https://nexus.akraino.org/content/repositories/snapshots/org/akraino/airshipinabottle_deploy/airshipinabottle_deploy/maven-metadata.xml | grep version | sed "s/.*<version>\([^<]*\)<\/version>.*/\1/" | head -4 | tail -1`
-airshipinabottle_deploy_build=`curl -s  https://nexus.akraino.org/content/repositories/snapshots/org/akraino/airshipinabottle_deploy/airshipinabottle_deploy/$airshipinabottle_deploy_version/maven-metadata.xml | grep '<value>' | head -1 | sed "s/.*<value>\([^<]*\)<\/value>.*/\1/"`
-airshipinabottle_deploy_tar=airshipinabottle_deploy-$airshipinabottle_deploy_build.tgz
-airshipinabottle_deploy_path="https://nexus.akraino.org/content/repositories/snapshots/org/akraino/airshipinabottle_deploy/airshipinabottle_deploy/$airshipinabottle_deploy_version"
-AIRSHIPINABOTTLE_URL="$airshipinabottle_deploy_path/$airshipinabottle_deploy_tar"
+rm -rf $SAMPLE_VNF_HOME
+mkdir -p $SAMPLE_VNF_HOME
+wget -q "$SAMPLE_VNF_URL" -O - | tar -xz -C $SAMPLE_VNF_HOME
 
 echo "Setting up airshipinabottle content/repositories"
-#checking if directory exist or not
-if [ -d $AIRSHIPINABOTTLE ]; then
-   rm -rf $AIRSHIPINABOTTLE
-   mkdir -p $AIRSHIPINABOTTLE
-else
-   mkdir -p $AIRSHIPINABOTTLE
-fi
-
-wget -q $AIRSHIPINABOTTLE_URL -O /opt/akraino/airshipinabottle_deploy.tgz
-tar -xzf /opt/akraino/airshipinabottle_deploy.tgz -C $AIRSHIPINABOTTLE
-rm -rf /opt/akraino/airshipinabottle_deploy.tgz
-
-REDFISH_HOME="/opt/akraino/redfish"
-redfish_version=`curl -s https://nexus.akraino.org/content/repositories/snapshots/org/akraino/redfish/redfish/maven-metadata.xml | grep version | sed "s/.*<version>\([^<]*\)<\/version>.*/\1/" | head -4 | tail -1`
-redfish_build=`curl -s  https://nexus.akraino.org/content/repositories/snapshots/org/akraino/redfish/redfish/$redfish_version/maven-metadata.xml | grep '<value>' | head -1 | sed "s/.*<value>\([^<]*\)<\/value>.*/\1/"`
-redfish_tar=redfish-$redfish_build.tgz
-redfish_path="https://nexus.akraino.org/content/repositories/snapshots/org/akraino/redfish/redfish/$redfish_version"
-REDFISH_HOME_URL="$redfish_path/$redfish_tar"
+rm -rf $AIRSHIPINABOTTLE_HOME
+mkdir -p $AIRSHIPINABOTTLE_HOME
+wget -q "$AIRSHIPINABOTTLE_URL" -O - | tar -xz -C $AIRSHIPINABOTTLE_HOME
 
 echo "Setting up sample vnf content/repositories"
-#checking if directory exist or not
-if [ -d $REDFISH_HOME ]; then
-   rm -rf $REDFISH_HOME
-   mkdir -p $REDFISH_HOME
-else
-   mkdir -p $REDFISH_HOME
-fi
+rm -rf $REDFISH_HOME
+mkdir -p $REDFISH_HOME
+wget -q "$REDFISH_URL" -O - | tar -xz -C $REDFISH_HOME
 
-wget -q $REDFISH_HOME_URL -O /opt/akraino/redfish.tgz
-tar -xzf /opt/akraino/redfish.tgz -C $REDFISH_HOME
-rm -rf /opt/akraino/redfish.tgz
 echo "SUCCESS:  Portal install completed"
+
