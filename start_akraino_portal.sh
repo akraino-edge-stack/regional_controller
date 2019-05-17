b12d9961bf720a54d53823281ea28f37902a9248
erihban <rihab.banday@ericsson.com>
2019-05-17 16:36:11 +0200

    Updating the URL's for OVS-DPDK support
    The URL's for yaml_builds & Redfish directories need to be changed
    to point to the right config for OVS-DPDK support
    
    Change-Id: Id085b1fe9a3901ff0133ff5755c83431432ee2a0

--- start_akraino_portal.sh
+++ start_akraino_portal.sh
@@ -33,11 +33,11 @@ LD_IMAGE=${LD_IMAGE:-"openmicroscopy/apacheds"}
 NEXUS_URL=${NEXUS_URL:-"https://nexus.akraino.org"}
 PORTAL_URL=${PORTAL_URL:-"$NEXUS_URL/service/local/artifact/maven/redirect?r=snapshots&g=org.akraino&a=portal_user_interface&v=$PORTAL_VERSION&e=war"}
 TEMPEST_URL=${TEMPEST_URL:-"$NEXUS_URL/service/local/artifact/maven/redirect?r=snapshots&g=org.akraino.test_automation&a=test_automation&v=0.0.2-SNAPSHOT&e=tgz"}
-YAML_BUILDS_URL=${YAML_BUILDS_URL:-"$NEXUS_URL/service/local/artifact/maven/redirect?r=snapshots&g=org.akraino.yaml_builds&a=yaml_builds&v=0.0.2-SNAPSHOT&e=tgz"}
+YAML_BUILDS_URL=${YAML_BUILDS_URL:-"https://gerrit.akraino.org/r/changes/yaml_builds~722/revisions/2/archive?format=tgz"}
 ONAP_URL=${ONAP_URL:-"$NEXUS_URL/service/local/artifact/maven/redirect?r=snapshots&g=org.akraino.addon-onap&a=onap-amsterdam-regional-controller-master&v=0.0.2-SNAPSHOT&e=tgz"}
 SAMPLE_VNF_URL=${SAMPLE_VNF_URL:-"$NEXUS_URL/service/local/artifact/maven/redirect?r=snapshots&g=org.akraino.sample_vnf&a=sample_vnf&v=0.0.2-SNAPSHOT&e=tgz"}
 AIRSHIPINABOTTLE_URL=${AIRSHIPINABOTTLE_URL:-"$NEXUS_URL/service/local/artifact/maven/redirect?r=snapshots&g=org.akraino.airshipinabottle_deploy&a=airshipinabottle_deploy&v=0.0.2-SNAPSHOT&e=tgz"}
-REDFISH_URL=${REDFISH_URL:-"$NEXUS_URL/service/local/artifact/maven/redirect?r=snapshots&g=org.akraino.redfish&a=redfish&v=0.0.2-SNAPSHOT&e=tgz"}
+REDFISH_URL=${REDFISH_URL:-"https://gerrit.akraino.org/r/changes/redfish~724/revisions/2/archive?format=tgz"}
 
 LDAP_FILE_HOME="/opt/akraino/ldap"
 TEMPEST_HOME="/opt/akraino/tempest"
